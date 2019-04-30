3afc33b2ea3e096909c84629637c551ac84a89ee
Ben Collins <bwcollins97@gmail.com>
2019-04-30 21:17:29 +1000

    Move ModuleInterface to package and Create LengthOfCode

--- src/main/java/modules/ModuleInterface.java
+++ src/main/java/modules/ModuleInterface.java
@@ -1,6 +1,11 @@
+package modules;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.SourceRoot;
+
 /**
  * Project      : Software Quality Assignment 1
- * Class name   : ModuleInterface
+ * Class name   : modules.ModuleInterface
  * Author(s)    : Kyle Fennell
  * Date         : 28/03/19
  * Purpose      : template interface that all modules will implement
@@ -13,7 +18,7 @@ public interface ModuleInterface {
     /**
      * @return results of the module being run
      */
-    String[] executeModule();
+    String[] executeModule(SourceRoot sourceRoot);
 
     /**
      * @return a description of what the module is testing
