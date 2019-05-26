48859b16c2772021a6b09c5b2e6e7ef9c4d5daaf
John Barr <john.j.barr@uon.edu.au>
2019-05-27 00:17:46 +1000

    initial commit for LocalVariables.

---
+++ src/main/java/modules/LocalVariables.java
@@ -0,0 +1,30 @@
+package modules;
+
+import com.github.javaparser.utils.SourceRoot;
+
+public class LocalVariables implements ModuleInterface {
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+
+	@Override
+	public String[] executeModule(SourceRoot sourceRoot) {
+		return new String[0];
+	}
+
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+
+
+	@Override
+	public String printMetrics() {
+		return null;
+	}
+
+}
